c7fa6ac4f6a97e5bc3068409b74d1c644db0c465
Vedran Novakovic <venovako@venovako.eu>
2026-01-17 04:28:43 +0100

    further updates

--- src/rowset.bat
+++ src/rowset.bat
@@ -1,2 +1,3 @@
-@ECHO OFF
-icx.exe /nologo /O3 /QxHost /DN=%1u %2 /Ferowset_%1.exe rowset.cpp /link /RELEASE
+FOR /L %%N IN (2,2,42) DO icx.exe /nologo /O3 /QxHost /DN=%%Nu /DNDEBUG /Ferowset_%%N.exe rowset.cpp /link /RELEASE
+IF EXIST timeit.dat DEL /F timeit.dat
+FOR /L %%N IN (2,2,42) DO "C:\Program Files (x86)\Windows Resource Kits\Tools\timeit.exe" rowset_%%N.exe > rowset_%%N.out 2> rowset_%%N.err
