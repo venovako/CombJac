f68fa93c254bfcc7ff716b592e809b494abfcf48
Vedran Novakovic <venovako@venovako.eu>
2026-01-18 03:23:53 +0100

    further updates

--- src/rowset.cpp
+++ src/rowset.cpp
@@ -21,6 +21,7 @@
 #endif /* ?_WIN32 */
 
 #include <algorithm>
+#include <chrono>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
@@ -133,19 +134,27 @@ static ushort indep_cnts[E_1], active_cnts[E_1];
 static ushort used_set[E], tmp_set[E], used_cnt, max_used_cnt;
 typedef unsigned long long ullong;
 static ullong btrack;
+typedef long double ldouble;
 
-static void make_in_strat()
+static bool make_in_strat()
 {
 #ifndef NDEBUG
   std::cerr << "Clearing memory... " << std::flush;
 #endif /* !NDEBUG */
-  (void)memset(in_strat, 0, sizeof(in_strat));
-  (void)memset(indep_sets, 0, sizeof(indep_sets));
-  (void)memset(active_sets, 0, sizeof(active_sets));
-  (void)memset(indep_cnts, 0, sizeof(indep_cnts));
-  (void)memset(active_cnts, 0, sizeof(active_cnts));
-  (void)memset(used_set, 0, sizeof(used_set));
-  (void)memset(tmp_set, 0, sizeof(tmp_set));
+  if (!memset(in_strat, 0, sizeof(in_strat)))
+    return false;
+  if (!memset(indep_sets, 0, sizeof(indep_sets)))
+    return false;
+  if (!memset(active_sets, 0, sizeof(active_sets)))
+    return false;
+  if (!memset(indep_cnts, 0, sizeof(indep_cnts)))
+    return false;
+  if (!memset(active_cnts, 0, sizeof(active_cnts)))
+    return false;
+  if (!memset(used_set, 0, sizeof(used_set)))
+    return false;
+  if (!memset(tmp_set, 0, sizeof(tmp_set)))
+    return false;
   max_used_cnt = used_cnt = 0u;
   btrack = 0ull;
 #ifndef NDEBUG
@@ -168,18 +177,54 @@ static void make_in_strat()
 #endif /* !NDEBUG */
 
 #ifndef NDEBUG
+  std::cerr << "Building forward-looking independent sets... " << std::flush;
+#endif /* !NDEBUG */
+  for (i = 0u; i < E_1; ++i)
+    for (ushort j = (i + 1u); j < E; ++j)
+      if ((in_strat[i].r != in_strat[j].r) && (in_strat[i].r != in_strat[j].c) && (in_strat[i].c != in_strat[j].r) && (in_strat[i].c != in_strat[j].c))
+        indep_sets[i][indep_cnts[i]++] = j;
+#ifndef NDEBUG
+  std::cerr << "done." << std::endl;
+#endif /* !NDEBUG */
+
+#ifndef NDEBUG
+  std::cerr << "Asserting monotonically non-increasing cardinalities... " << std::flush;
+#endif /* !NDEBUG */
+  for (i = 1u; i < E_1; ++i)
+    if (indep_cnts[i] > indep_cnts[i - 1u]) {
+#ifndef NDEBUG
+      std::cerr << "error!" << std::endl;
+#endif /* !NDEBUG */
+      return false;
+    }
+#ifndef NDEBUG
+  std::cerr << "done." << std::endl;
+#endif /* !NDEBUG */
+
+  return true;
+}
+
+static bool print_gv()
+{
+#ifndef NDEBUG
   std::cerr << "Describing the graph as a Graphviz file... " << std::flush;
 #endif /* !NDEBUG */
   std::ostringstream gv_filename;
   gv_filename << "rowset_" << N << ".gv";
   std::ofstream gv(gv_filename.str(), (std::ios::out | std::ios::trunc));
+  if (!gv) {
+#ifndef NDEBUG
+    std::cerr << "error!" << std::endl;
+#endif /* !NDEBUG */
+    return false;
+  }
   gv << "strict graph rowset {" << std::endl << "\tmargin=0" << std::endl;
 #ifndef GRAPHVIZ_SIZE
 #define GRAPHVIZ_SIZE 4.25
 #endif /* !GRAPHVIZ_SIZE */
   gv << "\tsize=" << GRAPHVIZ_SIZE << std::endl;
 #ifndef GRAPHVIZ_FONTSIZE
-#define GRAPHVIZ_FONTSIZE 48
+#define GRAPHVIZ_FONTSIZE 96
 #endif /* !GRAPHVIZ_FONTSIZE */
   gv << "\tnode [fontsize=" << GRAPHVIZ_FONTSIZE << ']' << std::endl;
 #ifndef GRAPHVIZ_EDGECOLOR
@@ -190,15 +235,15 @@ static void make_in_strat()
 #ifndef GRAPHVIZ_IXBASE
 #define GRAPHVIZ_IXBASE (ushort)1u
 #endif /* !GRAPHVIZ_IXBASE */
-  for (ushort j = 0u; j < i; ++j) {
-    pivot &pvt = in_strat[j];
-    gv << "\t\t" << (j + GRAPHVIZ_IXBASE) << " [label=\"" << (j + GRAPHVIZ_IXBASE) << "=(" << (pvt.r + GRAPHVIZ_IXBASE)  << ',' << (pvt.c + GRAPHVIZ_IXBASE) << ")\"]" << std::endl;
+  for (ushort i = 0u; i < E; ++i) {
+    pivot &pvt = in_strat[i];
+    gv << "\t\t" << (i + GRAPHVIZ_IXBASE) << " [label=\"" << (i + GRAPHVIZ_IXBASE) << "=(" << (pvt.r + GRAPHVIZ_IXBASE)  << ',' << (pvt.c + GRAPHVIZ_IXBASE) << ")\"]" << std::endl;
   }
   gv << "\t}" << std::endl;
-  for (ushort j = 0u; j < i; ++j)
-    for (ushort k = (j + 1u); k < i; ++k)
-      if ((in_strat[j].r == in_strat[k].r) || (in_strat[j].r == in_strat[k].c) || (in_strat[j].c == in_strat[k].r) || (in_strat[j].c == in_strat[k].c))
-        gv << "\t" << (j + GRAPHVIZ_IXBASE) << " -- " << (k + GRAPHVIZ_IXBASE) << std::endl;
+  for (ushort i = 0u; i < E_1; ++i)
+    for (ushort j = (i + 1u); j < E; ++j)
+      if ((in_strat[i].r == in_strat[j].r) || (in_strat[i].r == in_strat[j].c) || (in_strat[i].c == in_strat[j].r) || (in_strat[i].c == in_strat[j].c))
+        gv << "\t" << (i + GRAPHVIZ_IXBASE) << " -- " << (j + GRAPHVIZ_IXBASE) << std::endl;
   gv << "}" << std::endl;
   gv.close();
 #ifndef NDEBUG
@@ -212,32 +257,16 @@ static void make_in_strat()
 #endif /* !NDEBUG */
   std::ostringstream circo_call;
   circo_call << GRAPHVIZ_PREFIX << "circo -v -Tpdf -orowset_" << N << ".pdf -x rowset_" << N << ".gv > rowset_" << N << ".log 2>&1";
-  i = (ushort)system(circo_call.str().c_str());
+  if (system(circo_call.str().c_str())) {
 #ifndef NDEBUG
-  std::cerr << (i ? "error!" : "done.") << std::endl;
+    std::cerr << "error!" << std::endl;
+#endif /* !NDEBUG */
+    return false;
+  }
+#ifndef NDEBUG
+  std::cerr << "done." << std::endl;
 #endif /* !NDEBUG */
 #endif /* GRAPHVIZ_PREFIX */
-
-#ifndef NDEBUG
-  std::cerr << "Building forward-looking independent sets... " << std::flush;
-#endif /* !NDEBUG */
-  for (i = 0u; i < E_1; ++i)
-    for (ushort j = i + 1u; j < E; ++j)
-      if ((in_strat[i].r != in_strat[j].r) && (in_strat[i].r != in_strat[j].c) && (in_strat[i].c != in_strat[j].r) && (in_strat[i].c != in_strat[j].c))
-        indep_sets[i][indep_cnts[i]++] = j;
-#ifndef NDEBUG
-  std::cerr << "done." << std::endl;
-#endif /* !NDEBUG */
-
-#ifndef NDEBUG
-  std::cerr << "Asserting monotonically non-increasing cardinalities... " << std::flush;
-#endif /* !NDEBUG */
-  for (i = 1u; i < E_1; ++i)
-    if (indep_cnts[i] > indep_cnts[i - 1u])
-      exit(EXIT_FAILURE);
-#ifndef NDEBUG
-  std::cerr << "done." << std::endl;
-#endif /* !NDEBUG */
 
 #ifndef NDEBUG
   std::cerr << "Storing the forward-looking independent sets... " << std::flush;
@@ -245,11 +274,17 @@ static void make_in_strat()
   std::ostringstream txt_filename;
   txt_filename << "rowset_" << N << ".txt";
   std::ofstream txt(txt_filename.str(), (std::ios::out | std::ios::trunc));
-  for (i = 0u; i < E_1; ++i) {
-    txt << std::setw(maxw) << i << "=(" << std::setw(w) << ushort(in_strat[i].r) << ',' << std::setw(w) << ushort(in_strat[i].c) << ")[" << std::setw(maxw) << indep_cnts[i] << "]";
+  if (!txt) {
+#ifndef NDEBUG
+    std::cerr << "error!" << std::endl;
+#endif /* !NDEBUG */
+    return false;
+  }
+  for (ushort i = 0u; i < E_1; ++i) {
+    txt << std::setw(maxw) << (i + GRAPHVIZ_IXBASE) << "@(" << std::setw(w) << (in_strat[i].r + GRAPHVIZ_IXBASE) << ',' << std::setw(w) << (in_strat[i].c + GRAPHVIZ_IXBASE) << ")[" << std::setw(maxw) << indep_cnts[i] << "]";
     for (ushort j = 0u; j < indep_cnts[i]; ++j) {
       const ushort k = indep_sets[i][j];
-      txt << ", " << std::setw(maxw) << k << "=(" << std::setw(w) << ushort(in_strat[k].r) << ',' << std::setw(w) << ushort(in_strat[k].c) << ')';
+      txt << ", " << std::setw(maxw) << (k + GRAPHVIZ_IXBASE) << "=(" << std::setw(w) << (in_strat[k].r + GRAPHVIZ_IXBASE) << ',' << std::setw(w) << (in_strat[k].c + GRAPHVIZ_IXBASE) << ')';
     }
     txt << std::endl;
   }
@@ -257,6 +292,7 @@ static void make_in_strat()
 #ifndef NDEBUG
   std::cerr << "done." << std::endl;
 #endif /* ?NDEBUG */
+  return true;
 }
 
 static bool next_pivot()
@@ -339,13 +375,23 @@ static bool next_pivot()
   return false;
 }
 
-static void print_hdr()
+static bool print_hdr()
 {
+#ifndef NDEBUG
+  std::cerr << "Storing the strategy tables... " << std::flush;
+#endif /* !NDEBUG */
+
   std::ostringstream hdr_filename;
   hdr_filename << "rowset_" << N << ".hdr";
   std::ofstream hdr(hdr_filename.str(), (std::ios::out | std::ios::trunc));
+  if (!hdr) {
+#ifndef NDEBUG
+    std::cerr << "error!" << std::endl;
+#endif /* !NDEBUG */
+    return false;
+  }
   
-  hdr << "unsigned char rowcyc" << std::setfill('0') << std::setw(3) << N << std::setfill(' ')
+  hdr << "unsigned char cRC" << std::setfill('0') << std::setw(3) << N << std::setfill(' ')
       << '[' << std::setw(3) << S << "][" << std::setw(3) << P << "][2] =" << std::endl
       << '{' << std::endl;
 
@@ -365,17 +411,53 @@ static void print_hdr()
     hdr << std::endl;
   }
 
+  hdr << "};" << std::endl << std::endl;
+
+  hdr << "unsigned char RCc" << std::setfill('0') << std::setw(3) << N << std::setfill(' ')
+      << '[' << std::setw(3) << S << "][" << std::setw(3) << P << "][2] =" << std::endl
+      << '{' << std::endl;
+
+  i = E;
+  for (uchar s = S; s; ) {
+    hdr << "  {";
+    for (uchar p = P; p; ) {
+      const pivot &pvt = in_strat[used_set[--i]];
+      hdr << '{' << std::setw(w) << ushort(pvt.r) << ',' << std::setw(w) << ushort(pvt.c) << '}';
+      if (--p)
+        hdr << ',';
+    }
+    hdr << '}';
+    if (--s)
+      hdr << ',';
+    hdr << std::endl;
+  }
+
   hdr << "};" << std::endl;
   hdr.close();
+
+#ifndef NDEBUG
+  std::cerr << "done." << std::endl;
+#endif /* !NDEBUG */
+  return true;
 }
 
-static void print_idx()
+static bool print_idx()
 {
+#ifndef NDEBUG
+  std::cerr << "Storing the index tables... " << std::flush;
+#endif /* !NDEBUG */
+
   std::ostringstream idx_filename;
   idx_filename << "rowset_" << N << ".idx";
   std::ofstream idx(idx_filename.str(), (std::ios::out | std::ios::trunc));
- 
-  idx << "unsigned char rowidx" << std::setfill('0') << std::setw(3) << N << std::setfill(' ')
+  if (!idx) {
+#ifndef NDEBUG
+    std::cerr << "error!" << std::endl;
+#endif /* !NDEBUG */
+    return false;
+  }
+
+  idx << "unsigned char iRC" << std::setfill('0') << std::setw(3) << N << std::setfill(' ')
       << '[' << std::setw(3) << S << "][" << std::setw(3) << P << "] =" << std::endl
       << '{' << std::endl;
 
@@ -394,8 +476,158 @@ static void print_idx()
     idx << std::endl;
   }
 
+  idx << "};" << std::endl << std::endl;
+
+  idx << "unsigned char RCi" << std::setfill('0') << std::setw(3) << N << std::setfill(' ')
+      << '[' << std::setw(3) << S << "][" << std::setw(3) << P << "] =" << std::endl
+      << '{' << std::endl;
+
+  i = E;
+  for (uchar s = S; s; ) {
+    idx << "  {";
+    for (uchar p = P; p; ) {
+      idx << std::setw(maxw) << used_set[--i];
+      if (--p)
+        idx << ',';
+    }
+    idx << '}';
+    if (--s)
+      idx << ',';
+    idx << std::endl;
+  }
+
   idx << "};" << std::endl;
   idx.close();
+
+#ifndef NDEBUG
+  std::cerr << "done." << std::endl;
+#endif /* !NDEBUG */
+  return true;
+}
+
+static bool print_asy()
+{
+  static const ldouble zero = 0.0L;
+  static const ldouble one = 1.0L;
+  static const ldouble one_n = one / ldouble(N);
+  static const ldouble one_2n = one / ldouble(2u * N);
+  static const ushort size_pt = N * 30u;
+
+#ifndef NDEBUG
+  std::cerr << "Preparing the Asymptote visualization... " << std::flush;
+#endif /* !NDEBUG */
+
+  // draw images
+  ushort i = 0u;
+  for (uchar s = 0u; s < S; ++s) {
+    std::ostringstream asy_filename;
+    asy_filename << "rowset_" << N << '-' << std::setfill('0') << std::setw(w) << (s + GRAPHVIZ_IXBASE) << std::setfill(' ') << ".asy";
+    std::ofstream asy(asy_filename.str(), (std::ios_base::out | std::ios_base::trunc));
+    if (!asy) {
+#ifndef NDEBUG
+      std::cerr << "error!" << std::endl;
+#endif /* !NDEBUG */
+      return false;
+    }
+
+    asy << std::fixed << std::setprecision(17);
+
+    // header
+    asy << "usepackage(\"amssymb\");" << std::endl;
+    asy << std::endl;
+    asy << "size(" << size_pt << ");" << std::endl;
+    asy << std::endl;
+    asy << "draw(unitsquare);" << std::endl;
+    asy << std::endl;
+
+    // y isolines
+    for (uchar j = N_1; j; --j) {
+      const ldouble y = j * one_n;
+      asy << "draw((" << zero << ',' << y << ")--(" << one << ',' << y << "));" << std::endl;
+    }
+    asy << std::endl;
+
+    // x isolines
+    for (uchar j = N_1; j; --j) {
+      const ldouble x = j * one_n;
+      asy << "draw((" << x << ',' << zero << ")--(" << x << ',' << one << "));" << std::endl;
+    }
+    asy << std::endl;
+
+    // diagonal boxes
+    // D---C
+    // |   |
+    // A---B
+    for (uchar j = 0u; j < N; ++j) {
+      const uchar j1 = j + 1u;
+      const uchar n_j1 = N - j1;
+      const ldouble a_x = one_n * j;
+      const ldouble a_y = one_n * n_j1;
+      const ldouble b_x = a_x + one_n;
+      const ldouble b_y = a_y;
+      const ldouble c_x = b_x;
+      const ldouble c_y = b_y + one_n;
+      const ldouble d_x = a_x;
+      const ldouble d_y = c_y;
+      const ldouble l_x = a_x + one_2n;
+      const ldouble l_y = a_y + one_2n;
+      asy << "filldraw((" <<
+        a_x << ',' << a_y << ")--(" <<
+        b_x << ',' << b_y << ")--(" <<
+        c_x << ',' << c_y << ")--(" <<
+        d_x << ',' << d_y << ")--cycle,mediumgray);" << std::endl;
+      asy << "label(\"\\huge\\boldmath$" << (j + GRAPHVIZ_IXBASE) << "$\",(" << l_x << ',' << l_y << "),white);" << std::endl;
+    }
+
+    // step positions
+    for (uchar j = 0u; j < P; ++j) {
+      const pivot &pvt = in_strat[used_set[i++]];
+      const uchar p = pvt.r;
+      const uchar q = pvt.c;
+      ldouble l_x = __builtin_fmal(one_n, ldouble(q), one_2n);
+      ldouble l_y = one - __builtin_fmal(one_n, ldouble(p), one_2n);
+      asy << std::endl;
+      asy << "label(\"\\huge$\\blacksquare$\",(" << l_x << ',' << l_y << "));" << std::endl;
+      l_x = __builtin_fmal(one_n, ldouble(p), one_2n);
+      l_y = one - __builtin_fmal(one_n, ldouble(q), one_2n);
+      asy << "label(\"\\huge$\\blacksquare$\",(" << l_x << ',' << l_y << "),lightgray);" << std::endl;
+    }
+
+    asy.close();
+// use '-DASYMPTOTE_PREFIX=""' if asy is in the PATH, or '-DASYMPTOTE_PREFIX="asymptote_prefix/"' if it is not
+#ifdef ASYMPTOTE_PREFIX
+    std::ostringstream asy_call;
+    asy_call << ASYMPTOTE_PREFIX << "asy -v -f pdf -tex pdflatex " << asy_filename.str() << " >> rowset_" << N << ".log 2>&1";
+    if (system(asy_call.str().c_str())) {
+#ifndef NDEBUG
+      std::cerr << "error!" << std::endl;
+#endif /* !NDEBUG */
+      return false;
+    }
+#endif /* ASYMPTOTE_PREFIX */
+  }
+
+// use '-DPDFTK_PREFIX=""' if pdftk is in the PATH, or '-DPDFTK_PREFIX="pdftk_prefix/"' if it is not
+#ifdef ASYMPTOTE_PREFIX
+#ifdef PDFTK_PREFIX
+  std::ostringstream pdftk_call;
+  pdftk_call << PDFTK_PREFIX << "pdftk";
+  for (uchar s = 0u; s < S; ++s)
+    pdftk_call << " rowset_" << N << '-' << std::setfill('0') << std::setw(w) << (s + GRAPHVIZ_IXBASE) << std::setfill(' ') << ".pdf";
+  pdftk_call << " cat output rowset-" << N << ".pdf verbose dont_ask >> rowset_" << N << ".log 2>&1";
+  if (system(pdftk_call.str().c_str())) {
+#ifndef NDEBUG
+    std::cerr << "error!" << std::endl;
+#endif /* !NDEBUG */
+    return false;
+  }
+#endif /* PDFTK_PREFIX */
+#endif /* ASYMPTOTE_PREFIX */
+
+#ifndef NDEBUG
+  std::cerr << "done." << std::endl;
+#endif /* !NDEBUG */
+  return true;
 }
 
 int main(int argc, char *argv[])
@@ -405,20 +637,44 @@ int main(int argc, char *argv[])
     return EXIT_FAILURE;
   }
 
-  make_in_strat();
+  const auto start0 = std::chrono::high_resolution_clock::now();
+  if (!make_in_strat())
+    return EXIT_FAILURE;
+  const auto end0 = std::chrono::high_resolution_clock::now();
+  (void)print_gv();
 
 #ifndef NDEBUG
   std::cerr << "                                 " << std::flush;
 #endif /* !NDEBUG */
+  const auto start1 = std::chrono::high_resolution_clock::now();
   const bool ok = next_pivot();
+  const auto end1 = std::chrono::high_resolution_clock::now();
 #ifndef NDEBUG
   std::cerr << (ok ? " done." : " error!") << std::endl;
 #endif /* !NDEBUG */
   if (!ok)
     return EXIT_FAILURE;
 
-  print_hdr();
-  print_idx();
-  std::cout << "# of failed attempts = " << btrack << std::endl;
+  (void)print_hdr();
+  (void)print_idx();
+  (void)print_asy();
+
+#ifndef NDEBUG
+  std::cout << "# of failed attempts = ";
+#endif /* !NDEBUG */
+  std::cout << btrack;
+#ifdef NDEBUG
+  std::cout << ", ";
+#else /* !NDEBUG */
+  std::cout << " in ";
+#endif /* ?NDEBUG */
+  const auto ms = std::chrono::duration_cast<std::chrono::milliseconds>((end0 - start0) + (end1 - start1)).count();
+  std::cout << ms;
+#ifdef NDEBUG
+  std::cout << ", " << std::fixed << std::setprecision(9) << (ms ? __builtin_log10l(ldouble(ms) / 1000.0L) : -3.0L);
+#else /* !NDEBUG */
+  std::cout << " ms";
+#endif /* ?NDEBUG */
+  std::cout << std::endl;
   return EXIT_SUCCESS;
 }
